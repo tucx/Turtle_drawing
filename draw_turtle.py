17d63ecb6c72d80e90e268f9b7f96879a5b192b8
tucx <kikitutu@gmail.com>
2017-06-03 16:01:51 -0400

    second challenge

--- draw_turtle.py
+++ draw_turtle.py
@@ -4,6 +4,8 @@ def draw_square():
     window = turtle.Screen()
     window.bgcolor('red')
     brad = turtle.Turtle()
+    brad.shape('triangle')
+    brad.color('blue')
     #brad.forward(100)
     #brad.right(90)
     #brad.forward(100)
@@ -12,8 +14,19 @@ def draw_square():
     #brad.right(90)
     #brad.forward(100)
     #brad.right(90)
-    for i in range(8):
-        brad.forward(100)
-        brad.right(45)
+    #for i in range(8):
+    #    brad.forward(100)
+    #    brad.right(45)
+    brad.forward(120)
+    brad.left(135)
+    brad.forward(100)
+    brad.left(135)
+    brad.forward(100)
+    brad.left(135)
+    brad.forward(100)
+    brad.left(153)
+    brad.forward(135)
+
+    windows.exitonclick()
 
 draw_square()
