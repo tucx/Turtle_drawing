a6c51c3ccd21a2c4ac7e8a2eebc4486fa6878e6a
tucx <kikitutu@gmail.com>
2017-06-03 15:35:57 -0400

    Turtle drawing square

---
+++ draw_turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor('red')
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+draw_square()
